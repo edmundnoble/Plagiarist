d5d253f010445daa694eeb8b183c3557975da025
Edmund Noble <edmundnoble@gmail.com>
2012-02-07 08:04:58 -0500

    Work.

--- src/plagiarist/HelloWorldSwing.java
+++ src/plagiarist/HelloWorldSwing.java
@@ -3,6 +3,7 @@ package plagiarist;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -11,15 +12,15 @@ import java.awt.event.KeyEvent;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 public class HelloWorldSwing implements ActionListener {
-	private static String labelText = "Number of button clicks: ";
-	final JLabel label = new JLabel(labelText + "0    ");
-
+	// private static String labelText = "Number of button clicks: ";
+	// final JLabel label = new JLabel(labelText + "0    ");
+	JTextArea textArea = new JTextArea();
 	final static String LOOKANDFEEL = null;
 
 	/**
@@ -27,13 +28,19 @@ public class HelloWorldSwing implements ActionListener {
 	 * invoked from the event-dispatching thread.
 	 */
 	public Component createComponents() {
-		JButton button = new JButton("I'm a Swing button!");
+		JButton button = new JButton("Check for plagiarism");
 		button.setMnemonic(KeyEvent.VK_1);
 		button.addActionListener(this);
-		label.setLabelFor(button);
+		textArea.setFont(new Font("Serif", Font.ITALIC, 16));
+		textArea.setLineWrap(true);
+		textArea.setWrapStyleWord(true);
+
+		// label.setLabelFor(button);
 		JPanel pane = new JPanel(new GridLayout(0, 1));
+
+		pane.add(textArea);
+		// pane.add(label);
 		pane.add(button);
-		pane.add(label);
 		pane.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
 		return pane;
 	}
@@ -47,7 +54,7 @@ public class HelloWorldSwing implements ActionListener {
 		JFrame.setDefaultLookAndFeelDecorated(true);
 
 		// Create and set up the window.
-		JFrame frame = new JFrame("SwingApplication");
+		JFrame frame = new JFrame("Plagiarist");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		HelloWorldSwing app = new HelloWorldSwing();
@@ -71,7 +78,8 @@ public class HelloWorldSwing implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		System.out.print("");
+		Processing processor = new Processing();
+		System.out.print(processor.parseResults(textArea.getText()));
 	}
 
 	private static void initLookAndFeel() {
